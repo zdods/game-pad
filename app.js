dc0845a773981510d0d9df472c6ebab7650d63a4
zdods <z.dods@hotmail.com>
2019-04-15 20:42:22 -0400

    Init

---
+++ app.js
@@ -0,0 +1,78 @@
+// ********************
+// SERVER SETUP
+// ********************
+
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const io = require('socket.io').listen(http)
+
+const SerialPort = require('serialport')
+const parsers = SerialPort.parsers
+
+// USE bodyParser() -> get data from POST req
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
+// USE MORGAN LOGGER
+// app.use(morgan('dev'))
+
+// USE HELMET
+app.use(require('helmet')())
+
+// SERVE PUBLIC DIRECTORY
+app.use(express.static(__dirname + '/public'))
+
+// ********************
+// ROUTES FOR OUR API
+// ********************
+const router = express.Router()
+
+// MIDDLEWARE - auths 'n' stuff
+router.use((req, res, next) => {
+    next();
+})
+
+// REGISTER OUR ROUTES - prefix
+app.use('/api', router)
+
+// ********************
+// START THE SERVER
+// ********************
+
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+})
+
+var port = new SerialPort('/dev/cu.usbmodem14101', {
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+})
+
+port.pipe(parser)
+
+io.on('connection', function (socket) {
+    console.log('browser connected!')
+
+    socket.on('bind', (data) => {
+        port.write(JSON.stringify({data}))
+    })
+})
+
+parser.on('data', function (data) {
+    console.log(data)
+})
+
+// PORT
+const webPort = process.env.PORT || 1313 // set our port
+
+http.listen(webPort)
+console.log(`[PORT ${webPort}]: Is anyone there?`)
