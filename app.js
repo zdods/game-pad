e5d74d622ed816de04f5e3f5df1069b08ff7b5af
zdods <z.dods@hotmail.com>
2019-04-20 16:18:20 -0400

    Refine styles

--- app.js
+++ app.js
@@ -2,31 +2,29 @@
 // SERVER SETUP
 // ********************
 
-const express = require('express')
+const express = require("express")
 const app = express()
-const http = require('http').Server(app)
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const http = require("http").Server(app)
+const bodyParser = require("body-parser")
 
-const io = require('socket.io').listen(http)
+const io = require("socket.io").listen(http)
 
-const SerialPort = require('serialport')
+const SerialPort = require("serialport")
 const parsers = SerialPort.parsers
 
 // USE bodyParser() -> get data from POST req
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}))
+  })
+)
 app.use(bodyParser.json())
 
-// USE MORGAN LOGGER
-// app.use(morgan('dev'))
-
 // USE HELMET
-app.use(require('helmet')())
+app.use(require("helmet")())
 
 // SERVE PUBLIC DIRECTORY
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"))
 
 // ********************
 // ROUTES FOR OUR API
@@ -35,40 +33,40 @@ const router = express.Router()
 
 // MIDDLEWARE - auths 'n' stuff
 router.use((req, res, next) => {
-    next();
+  next()
 })
 
 // REGISTER OUR ROUTES - prefix
-app.use('/api', router)
+app.use("/api", router)
 
 // ********************
 // START THE SERVER
 // ********************
 
 const parser = new parsers.Readline({
-    delimiter: '\r\n'
+  delimiter: "\r\n"
 })
 
-var port = new SerialPort('/dev/cu.usbmodem14101', {
-    baudRate: 9600,
-    dataBits: 8,
-    parity: 'none',
-    stopBits: 1,
-    flowControl: false
+var port = new SerialPort("/dev/cu.usbmodem14101", {
+  baudRate: 9600,
+  dataBits: 8,
+  parity: "none",
+  stopBits: 1,
+  flowControl: false
 })
 
 port.pipe(parser)
 
-io.on('connection', function (socket) {
-    console.log('browser connected!')
+io.on("connection", function(socket) {
+  console.log("browser connected!")
 
-    socket.on('bind', (data) => {
-        port.write(JSON.stringify({data}))
-    })
+  socket.on("bind", data => {
+    port.write(JSON.stringify({ data }))
+  })
 })
 
-parser.on('data', function (data) {
-    console.log(data)
+parser.on("data", function(data) {
+  console.log(data)
 })
 
 // PORT
